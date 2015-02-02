83b4e0861da98edf8ec939c92007c78d79dacc22
Rachelle Keblitis <rkeblitis@gmail.com>
2015-02-02 12:37:08 -0800

    add npm http sever, add test ajax request to noms api, move html view to root directory

--- js/photos.js
+++ js/photos.js
@@ -1,1 +1,14 @@
-alert("Hello, world.");
+
+$( document ).ready(function() {
+// says when html is loaded, do the folowing:
+
+  $("#test").click(function() {
+    console.log("hi")
+    var $request = $(this)
+    $.ajax ({
+      url: "http://localhost:4000/foursquare"
+
+    });
+  })
+
+});

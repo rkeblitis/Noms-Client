98cbfece024d2dc8dbf3406dbc3b855a9ce68023
Rachelle Keblitis <rkeblitis@gmail.com>
2015-02-06 13:29:32 -0800

    add geolocation stuff

--- js/photos.js
+++ js/photos.js
@@ -1,25 +1,55 @@
-
-$( document ).ready(function() {
 // says when html is loaded, do the folowing:
+$( document ).ready(function() {
 
-  $("#picDecision"). click(function() {
+  if ("geolocation" in navigator) {
+    /* geolocation is available */
+  } else {
+    /* geolocation IS NOT available */
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    do_something(position.coords.latitude, position.coords.longitude);
+  });
+
+
+  $("#picDecision").submit(function() {
     var $request = $(this)
     $.ajax ({
       url: "http://localhost:4000/foursquare",
-      type: "GET",
+      type: "POST",
       dataType: "json",
       success: function (data) {
-        console.log(data)
+        console.log(data);
         // var obj = jQuery.parseJSON( data )
-        var element = $("<img />")
+        // console.log($(request)attr.id);
+
+        var element = $("<img />");
         element.attr("src", data.url);
         $("#pics").html(element);
       },
-
       error: function () {
         console.log("ErrrROAR");
       }
+
     });
+
+    function blah () {
+      console.log("blah");
+    }
   })
+  //
+  // $("menButton").click(function() {
+  //   $.ajax ({
+  //     url: "http://localhost:4000/foursquare",
+  //     type: "POST",
+  //     dataType: "html"
+  //     success: function () {
+  //       console.log()
+  //     }
+  //   });
+  // })
 
 });
+
+
+// on success

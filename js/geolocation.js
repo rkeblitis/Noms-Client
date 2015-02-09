2841d0bcf8fb7b7cf89be137054e546afad7be42
Rachelle Keblitis <rkeblitis@gmail.com>
2015-02-08 16:05:54 -0800

    add error to geolocation post request

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,22 +1,31 @@
-//why u mad?!?!
+//Get User Location
 $( document ).ready(function() {
   var geoSuccess = function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
-    console.log(position);
+    console.log(lat);
     $.ajax({
       type: "POST",
       url: "http://localhost:4000/foursquare",
       data: {
           lat: lat,
           lon: lon
-
       },
       dataType: "json",
       success: function(data) {
         console.log(data);
+        var obj = data
+        var element = $("<img />");
+        element.attr("src", data.url);
+        $("#pics").html(element);
+      },
+
+      error: function() {
+        console.log("ErrrROAR");
       }
+
+
     });
   };
 
--- js/photos.js
+++ js/photos.js
@@ -1,41 +1,30 @@
-// says when html is loaded, do the folowing:
 $( document ).ready(function() {
-
   $("#picDecision").submit(function() {
     var $request = $(this)
-    // $.ajax ({
-    //   url: "http://localhost:4000/foursquare",
-    //   type: "GET",
-    //   dataType: "json",
-    //   success: function (data) {
-    //     console.log(data);
-    //     // var obj = jQuery.parseJSON( data )
-    //     // console.log($(request)attr.id);
-    //
-    //     var element = $("<img />");
-    //     element.attr("src", data.url);
-    //     $("#pics").html(element);
-    //   },
-    //   error: function () {
-    //     console.log("ErrrROAR");
-    //   }
-    //
-    // });
-    //
-    // function blah () {
-    //   console.log("blah");
-    // }
-  })
-  //
-  // $("menButton").click(function() {
-  //   $.ajax ({
-  //     url: "http://localhost:4000/foursquare",
-  //     type: "POST",
-  //     dataType: "html"
-  //     success: function () {
-  //       console.log()
-  //     }
-  //   });
-  // })
+    console.log($request)
+    console.log(obj)
+//     $.ajax ({
+//       type: "POST",
+//       url: "http://localhost:4000/foursquare",
+//       data: {
+//         reaction: $resuest
+//       },
+//       success: function (data) {
+//         dataType: "json",
+//         console.log(data);
+//         var element = $("<img />");
+//         element.attr("src", data.url);
+//         $("#pics").html(element);
+//       },
+//       error: function () {
+//         console.log("ErrrROAR");
+//       }
+//
+//     });
+//
+//     function blah () {
+//       console.log("blah");
+//     }
+   })
 
 });
